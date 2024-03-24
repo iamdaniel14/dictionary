212b3c5305d6a21dbd1abfb7412e36a0cbdc2da7
iamdaniel14 <96418140+iamdaniel14@users.noreply.github.com>
2024-03-24 19:21:29 +0100

    second commit

--- dictionary.js
+++ dictionary.js
@@ -3,30 +3,43 @@
 const MAIN_CONTAINER=document.querySelector ("#main-container");
 let word_input= document.querySelector ("#wordInput");
 const SEARCH_BUTTON =document.querySelector ('#searchButton');
+const SEARCHED_WORD=document.querySelector("#searchedWord");
 
 SEARCH_BUTTON.addEventListener ("click", ()=>{
-console.log (word_input.value);
 let word=(word_input.value).trim ().toLowerCase();
 words (word);
+SEARCHED_WORD.innerHTML=word;
+SEARCHED_WORD.classList.add ("searched-word");
 
 });
-// console.log (word_input.value);
+
 async function words (word){
+try { 
 let Response= await fetch (`https://api.wordnik.com/v4/word.json/${word}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=c1raqaa3pes2o7qqqkgrrgmtv4qe9qx181le2gy2xqs148ief`);
 let data = await Response.json();
-console.log (data);
-showDefinition (data)
+
+ 
+let filteredData=data.filter(definition=>{ 
+ return ( typeof definition.text !=="undefined") ; //return true if data is not undefined
+ })
+
+ displayData (filteredData); //call the displayData function
+
+} catch (err){
+console.error(err);
+console.log ("data is not fetch")
+}
 }
 
 
-function showDefinition (wordData){
 
+function displayData (filteredData){
  MAIN_CONTAINER.innerHTML="";
- wordData.forEach ((wordDefinition) => {
+ filteredData.forEach ((wordDefinition) => {
  const SUB_CONTAINER=document.createElement ('div');
  SUB_CONTAINER.classList.add ("sub-container");
 
- if (wordDefinition.text!==undefined) {  //if the definition is not undefined
+//  if (wordDefinition.text!==undefined) {  //if the definition is not undefined
  let word=document.createElement('p');
  word.innerHTML=wordDefinition.word;
  word.classList.add ('word');
@@ -42,9 +55,6 @@ function showDefinition (wordData){
  p.innerHTML=wordDefinition.text;
  p.classList.add ('paragraph');
  SUB_CONTAINER.appendChild (p);
-
-}
-
  MAIN_CONTAINER.appendChild(SUB_CONTAINER);
 
 });
