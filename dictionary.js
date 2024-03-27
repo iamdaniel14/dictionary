2788b22d5ea5ed36bce992c13b2265ee41c08685
iamdaniel14 <96418140+iamdaniel14@users.noreply.github.com>
2024-03-27 20:34:09 +0100

    third commit

--- dictionary.js
+++ dictionary.js
@@ -4,12 +4,15 @@ const MAIN_CONTAINER=document.querySelector ("#main-container");
 let word_input= document.querySelector ("#wordInput");
 const SEARCH_BUTTON =document.querySelector ('#searchButton');
 const SEARCHED_WORD=document.querySelector("#searchedWord");
+const RELATED_WORDS_CONTAINER=document.querySelector(".related-words-container");
+
 
 SEARCH_BUTTON.addEventListener ("click", ()=>{
 let word=(word_input.value).trim ().toLowerCase();
 words (word);
 SEARCHED_WORD.innerHTML=word;
 SEARCHED_WORD.classList.add ("searched-word");
+
 
 });
 
@@ -18,6 +21,11 @@ try {
 let Response= await fetch (`https://api.wordnik.com/v4/word.json/${word}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=c1raqaa3pes2o7qqqkgrrgmtv4qe9qx181le2gy2xqs148ief`);
 let data = await Response.json();
 
+
+let ResponseRelatedWord=await fetch (`https://api.wordnik.com/v4/word.json/${word}/relatedWords?useCanonical=false&limitPerRelationshipType=10&api_key=c1raqaa3pes2o7qqqkgrrgmtv4qe9qx181le2gy2xqs148ief`);
+let relatedWordsJason = await ResponseRelatedWord.json();
+// console.log (relatedWordsJason);
+showRelatedWord(relatedWordsJason);
  
 let filteredData=data.filter(definition=>{ 
  return ( typeof definition.text !=="undefined") ; //return true if data is not undefined
@@ -59,4 +67,33 @@ function displayData (filteredData){
 
 });
 
-}
+ }
+
+ function showRelatedWord(relatedWords){
+
+ RELATED_WORDS_CONTAINER.innerHTML="";
+relatedWords.forEach ((relatedWordsArray,index)=>{
+
+let words_container=document.createElement ("div");
+words_container.classList.add ("words_container");
+console.log(words_container);
+
+ console.log (index);
+ let relationshipType=document.createElement ("h1");
+ relationshipType.classList.add ("relationship-type")
+ relationshipType.innerHTML=`relationship type ${relatedWordsArray.relationshipType}`;
+ words_container.appendChild(relationshipType);
+ 
+console.log (`relationship type ${relatedWordsArray.relationshipType}`);
+relatedWordsArray.words.forEach((word)=>{
+let similar=document.createElement ("p");
+similar.classList.add ("similar-word")
+similar.innerHTML=`${word}`;
+words_container.appendChild(similar);
+});
+
+RELATED_WORDS_CONTAINER.appendChild(words_container);
+
+})
+
+ }
